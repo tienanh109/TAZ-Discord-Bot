8604b4aed98d31a56d572b6d24cea54d5b537acc
Tien Anh <99895433+tienanh109@users.noreply.github.com>
2024-06-14 22:18:56 +0700

    unzipgit

---
+++ Cogs/System/restart.py
@@ -0,0 +1,21 @@
+print("Loading restart...")
+import discord
+import os
+import asyncio
+import sys
+from dotenv import load_dotenv
+from discord.ext import commands
+
+load_dotenv()
+OWNER = os.getenv("OWNER_USERNAME")
+
+def setup(bot):
+    @bot.command()
+    async def restart(ctx):
+      if ctx.author.name == OWNER:
+          await ctx.reply("Will do it as soon as possible!")
+          await asyncio.sleep(1)
+          python = sys.executable
+          os.execl(python, python, *sys.argv)
+      else:
+        await ctx.reply("You do not have permission to execute this command!")
