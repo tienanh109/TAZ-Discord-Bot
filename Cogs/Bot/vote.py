bbc3bab6bf50a116163c48cd54ade73356dd6171
Tien Anh <99895433+tienanh109@users.noreply.github.com>
2024-06-15 20:57:37 +0700

    Update vote.py

--- Cogs/Bot/vote.py
+++ Cogs/Bot/vote.py
@@ -6,7 +6,7 @@ class VoteCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.slash_command(description="Show list of commands")
+    @commands.slash_command(description="Show vote link top.gg of TAZ Bot")
     async def vote(self, ctx):
         commands_list = "> You can vote for us at: \n> https://discord.com/oauth2/authorize?client_id=926795496469704765&permissions=8&scope=bot \n> ***Thank you so much 💖!***"
         await ctx.respond(commands_list)
